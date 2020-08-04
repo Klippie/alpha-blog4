dea268d9976cabc988d869c0d94fb3eb11c2c60e
Ubuntu <ubuntu@ip-172-31-4-159.eu-west-2.compute.internal>
2020-08-04 18:37:02 +0000

    Make home and about page with their routes and controller

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.hosts << "8954c90e22c549c4a22423d4eab81284.vfs.cloud9.eu-west-2.amazonaws.com"
 end
