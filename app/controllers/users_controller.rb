3a01647b2b76775110bf1caa9a32ad8a823b463f
Ubuntu <ubuntu@ip-172-31-4-159.eu-west-2.compute.internal>
2020-08-13 19:16:42 +0000

    Add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def edit
+    @user = User.find(params[:id])  
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to articles_path
+    else
+      flash[:notice] = ""
+    end
+  end
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -12,6 +26,8 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+    
+    
   end
   
   private
